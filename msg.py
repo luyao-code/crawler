64df6307dcd96c2468cd852ff28f648ad0cb7058
yaolu <lu.yao@outlook.com>
2023-11-20 23:54:07 -0500

    Modify

--- msg.py
+++ msg.py
@@ -1,16 +1,15 @@
 from twilio.rest import Client
 
 # Twilio config
-ACCOUNT_SID = 'AC55463228a82c7bcdf8aca17873ce1312'
-AUTH_TOKEN = 'd97c324c1da5b0db2adb5ffb0e62b063'
-FROM_NUMBER = '+15209996998'
+FROM_NUMBER = '+1234567890'
 
 class Messager:
     def __init__(self):
-        self.client = Client(ACCOUNT_SID, AUTH_TOKEN)
-        # self.account_sid = os.environ['TWILIO_ACCOUNT_SID']
-        # self.auth_token = os.environ['TWILIO_AUTH_TOKEN']
-        self.to_number = '+18572723202'
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        to_number = os.environ['TWILIO_TO_NUMBER']
+        self.client = Client(account_sid, auth_token)
+        self.to_number = to_number
 
     def send_message(self, msg):
         message = self.client.messages.create(
